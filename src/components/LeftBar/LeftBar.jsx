24bb90795293bdfc898f1bb85b12503ac92ac9ad
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-10-20 08:30:21 +0000

    make right navbar

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -6,6 +6,7 @@ export default function LeftBar() {
       <section>
         <div className="items">
           <img
+            className="profile"
             src=" https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
             alt="Profile"
           />
--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,6 +1,150 @@
-import './RightBar.scss';
+import "./RightBar.scss";
 
-export default function RightBar(){
-
-    return (<div className="rightBar">RightBar</div>)
+export default function RightBar() {
+  return (
+    <aside className="rightBar">
+      <section className="items">
+        <h6>Suggestions For You</h6>
+        <div className="contents">
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <span>Nial Haran</span>
+            </div>
+            <div className="right">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <span>Nial Haran</span>
+            </div>
+            <div className="right">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="items">
+        <h6>Latest Activities</h6>
+        <div className="contents">
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <h6>
+                Nial Haran
+                <span> changed their cover picture</span>
+              </h6>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <h6>
+                Nial Haran <span> like a post</span>
+              </h6>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <h6>
+                Nial Haran <span> do comment</span>
+              </h6>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <img
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                alt="profile pic"
+              />
+              <h6>
+                Nial Haran <span> post</span>
+              </h6>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="items">
+        <h6>Online Friends</h6>
+        <div className="contents">
+          <div>
+            <div className="left">
+              <div className="online">
+                <img
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="profile pic"
+                />
+              </div>
+              <h6>Nial Haran</h6>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <div className="online">
+                <img
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="profile pic"
+                />
+              </div>
+              <h6>Nial Haran</h6>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <div className="online">
+                <img
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="profile pic"
+                />
+              </div>
+              <h6>Nial Haran</h6>
+            </div>
+          </div>
+          <div>
+            <div className="left">
+              <div className="online">
+                <img
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="profile pic"
+                />
+              </div>
+              <h6>Nial Haran</h6>
+            </div>
+          </div>
+        </div>
+      </section>
+    </aside>
+  );
 }
